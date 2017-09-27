ab859827f24e8eb09c2e15340f3e7328f3dee71e
dvinubius <dvinubiu.narmon@gmail.com>
2017-09-27 17:08:27 +0300

    Game fully functional according to specs. Added ToDo doc for future improvement. game offers two types of questions, but not yet choosable by user (hardcoded). In this version, the type of questions served is 	square root questions. Improved styling.

--- code/main.js
+++ code/main.js
@@ -2,13 +2,15 @@ var gameState; // 'ready', 'playing', 'over'
 
 var correctAnswer;
 
-var counter;
+var countDown;
+var timer;
 var question;
 var choices;
 var start_reset_button;
 var scoreDisplay;
-var scoreValue;
+var scoreValueDisplay;
 var timeDisplay;
+var timeValueDisplay;
 var feedbackCorrect;
 var feedbackWrong;
 var gameOverDisplay;
@@ -25,28 +27,29 @@ function initGlobals() {
   choices = document.getElementById('choices');
   start_reset_button = document.getElementById('start_reset');
   scoreDisplay = document.getElementById('score');
-  scoreValue = document.getElementById('scoreValue');
-  timeDisplay = document.getElementById('timeDisplay');
+  scoreValueDisplay = document.getElementById('scoreValue');
+  timeDisplay = document.getElementById('timer');
+  timeValueDisplay = document.getElementById('timeValue');
   feedbackCorrect = document.getElementById('feedbackCorrect');
   feedbackWrong = document.getElementById('feedbackWrong');
   gameOverDisplay = document.getElementById('gameOver');
-  finalScoreDisplay = document.getElementById('finalScoreDisplay');
+  finalScoreDisplay = document.getElementById('finalScore');
 
   // AUX for resetGame()
   scoreDisplay.reset = function() {
-    scoreValue = 0;
     this.style.visibility = 'visible';
+    scoreValueDisplay.innerHTML = '0';
   }
   // AUX for resetGame()
   timeDisplay.reset = function() {
-    counter=60;
     this.style.visibility = 'visible';
+    timeValueDisplay.innerHTML = countDown;
   }
   // AUX for resetGame()
   choices.reset = function() {
     squares = choices.children;
     for (var i=0; i<squares.length; i++) {
-      squares[i].textContent = 0;
+      squares[i].textContent = '';
       squares[i].classList.add('resetChoice');
     }
   }
@@ -62,12 +65,15 @@ function initGlobals() {
 }
 
 function resetGame() {
+  countDown = 60;
+  window.clearInterval(timer);
   gameState = 'ready';
   question.textContent = '';
   choices.reset();
   start_reset_button.innerHTML = 'Start Game';
   scoreDisplay.reset();
   timeDisplay.reset();
+  gameOverDisplay.style.visibility = 'hidden';
 }
 
 function startGame() {
@@ -80,43 +86,40 @@ function startGame() {
 
 function finishGame() {
   gameState = 'over';
-  finalScoreDisplay = scoreValue;
+  finalScoreDisplay.textContent = scoreValueDisplay.textContent;
   gameOverDisplay.style.visibility = 'visible';
   scoreDisplay.style.visibility = 'hidden';
   timeDisplay.style.visibility = 'hidden';
+  start_reset_button.innerHTML = 'Play Again';
 }
 
 /* Generate a new question and init UI (question & choices) with the values */
 function nextQuestion() {
-  // generate values
-  var qText = '5x5'; // TODO develop
-  correctAnswer = '25'; // TODO develop
-  // set question in UI
-  question.textContent = qText;
+  // generate question
+// randomMultiplyQuestion();
+  randomSqrtQuestion();
   // set the choices in UI
-  initChoices();
-}
-/* Randomly place the answer value in one of the four choice boxes
-   and set random values for in other ones */
-function initChoices() {
-  // which choice should contain the correct answer?
-  var correctSquare = Math.floor(Math.random()*4); // 0, 1, 2 or 3
-  // set the choices in UI
-  var squares = choices.children;
-  for (var i=0; i<squares.length; i++) {
-    squares[i].textContent = (i == correctSquare) ? correctAnswer : randomAnswer();
-  }
+//  initChoicesMultiplyQuestion();
+  initChoicesSqrtQuestion();
 }
 
 // Starts the countdown. When finished, finishes the game
 function startCountdown() {
-  // ... begin
-  // when over, finishGame();
+  var timerAction = () => {
+    if (countDown > 0) {
+      countDown--;
+      timeValueDisplay.textContent = countDown;
+    } else { // countDown == 0
+      finishGame();
+      window.clearInterval(timer);
+    }
+  };
+  timer = window.setInterval(timerAction, 1000);
 }
 
 // Handler for clicks on the start_reset_button
-function clickOnStartReset() {
-  gameState=='ready' ? startGame() : reset();
+function clickOnStart_Reset() {
+  gameState=='ready' ? startGame() : resetGame();
 }
 
 // Handler for clicks on choice boxes
@@ -125,13 +128,65 @@ function clickOnChoice(target) {
   if (! (gameState=='playing')) return;
   // else
   if (target.textContent == correctAnswer) {
-    // ...rightAnswer();
-  } else {
-    // ...wrongAnswer();
+    scoreValueDisplay.textContent = (Number(scoreValueDisplay.textContent) + 1);
+    feedbackCorrect.style.visibility = 'visible';
+    setTimeout(() => {feedbackCorrect.style.visibility = 'hidden';}, 500);
+    nextQuestion();
+  } else { // wrong answer
+    feedbackWrong.style.visibility = 'visible';
+    setTimeout(() => {feedbackWrong.style.visibility = 'hidden';}, 300);
   };
 }
 
-// AUX function
-function randomAnswer() {
-  return Math.random() * 100;
+// AUX function that generates a question of multiplication of 2 integers
+// between 1 and 10 (including). Also sets the global correctAnswer and
+// loads the text into the UI
+function randomMultiplyQuestion() {
+  // generate random values
+  var f1 = Math.ceil(Math.random() * 10);
+  var f2 = Math.ceil(Math.random() * 10);
+  var qText = f1+'x'+f2;
+  correctAnswer = f1*f2;
+  // set question in UI
+  question.textContent = qText;
+}
+// AUX function that generates a question of square root for an integer
+// between 0 and 200. Also sets the global correctAnswer and
+// loads the text into the UI
+function randomSqrtQuestion() {
+  var initial = Math.floor(Math.random() * (625+1));
+  question.innerHTML = '&radic;'+initial;
+  correctAnswer = (Math.sqrt(initial)).toFixed(2);
+}
+
+/* Randomly place the answer value in one of the four choice boxes
+   and set proper random values for in other ones */
+function initChoicesMultiplyQuestion() {
+  // which choice should contain the correct answer?
+  var correctSquare = Math.floor(Math.random()*4); // 0, 1, 2 or 3
+  // set the choices in UI
+  var squares = choices.children;
+  for (var i=0; i<squares.length; i++) {
+    if (i == correctSquare) {
+      squares[i].textContent =  correctAnswer;
+    } else {
+      squares[i].textContent = Math.round(Math.random() * 100);
+    }
+  }
+}
+
+/* Randomly place the answer value in one of the four choice boxes
+   and set proper random values (2 decimals precision) in other ones */
+function initChoicesSqrtQuestion() {
+  // which choice should contain the correct answer?
+  var correctSquare = Math.floor(Math.random()*4); // 0, 1, 2 or 3
+  // set the choices in UI
+  var squares = choices.children;
+  for (var i=0; i<squares.length; i++) {
+    if (i == correctSquare) {
+      squares[i].textContent =  correctAnswer;
+    } else {
+      squares[i].textContent = (Math.random() * (25+1)).toFixed(2);
+    }
+  }
 }
